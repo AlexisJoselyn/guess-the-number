1b6f64666f9d6bbe2ad77936903b427700f1f939
AlexisJoselyn <pfernandez.alexis@gmail.com>
2024-04-04 01:20:35 -0500

    chore:add contructors and instance HumanPlayer

--- src/GuessTheNumberGame.java
+++ src/GuessTheNumberGame.java
@@ -1,16 +1,24 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class GuessTheNumberGame {
 
     public static void main(String[] args) {
+        ArrayList<Integer> guesses = new ArrayList<>();
+        guesses.add(9);
+
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int targetNumber = random.nextInt(100) + 1;
 
+        System.out.println("<<GUESS THE NUMBER>>");
         System.out.print("Por favor, ingresa tu nombre: ");
         String playerName = scanner.nextLine();
 
-        System.out.println("¡Hello!, " + playerName + " welcome to Guess the number - " + targetNumber);
+        HumanPlayer humanPlayer = new HumanPlayer(playerName,guesses);
+        humanPlayer.makeGuess();
+
     }
 
     public boolean checkGuess (Player player) {
--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class HumanPlayer extends Player{
+    public HumanPlayer(String name, ArrayList<Integer> guesses) {
+        super(name, guesses);
+    }
+
     public int makeGuess () {
+        System.out.println("Hola, " + getName() + ". ¡Juguemos!");
         return 1;
     }
 }
